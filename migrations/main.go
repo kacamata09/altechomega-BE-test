8a4b2988ffb77d547c305edbe231f55ce09a8255
Muh. Anshar Azhari <muh.ansharazhari@gmail.com>
2024-12-10 20:46:55 +0700

    feat: add app for migrations

--- migrations/main.go
+++ migrations/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("yaa")
+	viper.SetConfigFile("../env.yaml")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	dbHost := viper.GetString("database.host")
+	dbUser := viper.GetString("database.username")
+	dbPort := viper.GetInt("database.port")
+	dbPass := viper.GetString("database.password")
+	dbName := viper.GetString("database.name")
+
+	connection := fmt.Sprintf("mysql://%s:%s@tcp(%s:%d)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	m, err := migrate.New(
+
+		"file://mysql",
+		connection,
+	)
+
+	if err != nil {
+
+		log.Fatalf("Failed to create migrate: %v", err)
+
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+
+		log.Fatalf("Failed to apply migrations: %v", err)
+
+	}
+
+	fmt.Println("Migrations applied successfully!")
+
 }
